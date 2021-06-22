29ccc9d705fccf2ae4a83fd6e92a9f601bc90bc9
Carlos Duque <carlosduque.hn@gmail.com>
2021-06-22 18:15:47 -0400

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to torneoencasa
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/torneoencasa/core.clj
@@ -0,0 +1,7 @@
+(ns torneoencasa.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
